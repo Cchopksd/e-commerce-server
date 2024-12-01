08e28932857f14279e110e440e60210d63b2f830
chopper <kasidit.suwa@gmail.com>
2024-12-01 14:57:40 +0700

    install ngrok

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import * as morgan from 'morgan';
-
+import * as ngrok from '@ngrok/ngrok';
 import helmet from 'helmet';
 
 async function bootstrap() {
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.use(morgan('dev'));
   app.enableCors({
-    origin: ['http://127.0.0.1:3000'],
+    origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
     credentials: true,
   });
   app.use(helmet());
@@ -23,6 +23,22 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 5500);
+  const port = process.env.PORT ?? 5500;
+  await app.listen(port);
+  console.log(`Application is running on http://localhost:${port}`);
+
+  try {
+    // Establish ngrok tunnel
+    const listener = await ngrok.connect({
+      addr: port,
+      authtoken: process.env.NGROK_AUTHTOKEN,
+      subdomain: 'pika-perfect-gar',
+      region: 'th',
+    });
+    console.log(`Ingress established at: ${listener.url()}`);
+  } catch (err) {
+    console.error('Error establishing ngrok tunnel:', err.message);
+  }
 }
+
 bootstrap();
